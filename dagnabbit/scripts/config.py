4f674a00d7400447d38b4df8bb08afb8a5d8cf2a
Kai Christensen <matthew.kai.christensen@gmail.com>
2026-06-13 12:34:33 -0600

    restore lr for fair comparison

--- dagnabbit/scripts/config.py
+++ dagnabbit/scripts/config.py
@@ -20,7 +20,7 @@ MCMC_PASSES_PER_SPLIT_ROUND = 2
 # --- training ---
 NUM_STEPS = 1_000_000
 GRADIENT_ACCUMULATION_STEPS = 8
-LEARNING_RATE = 3e-4
+LEARNING_RATE = 1e-3
 
 # Max L2 norm of gradients across all parameters before each optimizer step.
 # Set to None to disable clipping.
